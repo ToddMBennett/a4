9c64aca8b95d6b3614aa5db64db026c8e3e62209
Todd Bennett <todd.m.bennett@gmail.com>
2017-04-21 18:57:41 +0900

    adding basic routing, controller files

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/','CalcController@show');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+if(config('app.env') == 'local') {
+  Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+}
